c5979fba0b16b5f1167f120b271b1943f86c0621
codeonium <codeon2020@gmail.com>
2021-03-04 15:36:15 +0000

    second commit

--- high_scores/src/high_scores.py
+++ high_scores/src/high_scores.py
@@ -1,10 +1,30 @@
-def latest(scores):
-    pass
 
+def latest(scores):
+    return scores[-1]
 
 def personal_best(scores):
-    pass
+    return sorted(scores)[-1]
 
 
 def personal_top_three(scores):
-    pass
+    sorted_scores = sorted(scores)
+    return sorted_scores[-3:][::-1]
+
+def low_to_high(scores):
+    return sorted(scores)[::-1]
+
+def if_tie(scores):
+    top_scores_filtered = []
+    for scores in scores:
+        if scores not in top_scores_filtered:
+            top_scores_filtered.append(scores)
+    return top_scores_filtered
+    #top_scores_filtered = [score for score in scores if scores.append(scores)]
+    #if scores include 2 the same numbers
+    #return top 3 scores
+
+
+# top_scores_filtered = []
+# for scores in scores:
+#         if scores not in top_scores_filtered:
+#             top_scores_filtered.append(scores)
--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -1,21 +1,25 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import *
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
 
 class HighScoresTest(unittest.TestCase):
+    def setUp(self):
+        self.score_list = [1, 23, 65, 12, 99, 22, 65, 9]
     
-    # Tests
+    def test_latest(self):
+        self.assertEqual(9, latest(self.score_list))
+        
+    def test_best(self):
+        self.assertEqual(99, personal_best(self.score_list))
 
-    # Test latest score (the last thing in the list)
+    def test_three_best(self):
+        self.assertEqual([99, 65, 23], personal_top_three(self.score_list))
 
-    # Test personal best (the highest score in the list)
-
-    # Test top three from list of scores
-
-    # Test ordered from highest tp lowest
+    def test_order_from_lowest(self):
+        self.assertEqual([99, 65, 23, 22, 12, 9, 1], low_to_high(self.score_list))
 
     # Test top three when there is a tie
 
